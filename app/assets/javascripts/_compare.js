170352b00f3b2e2986fe7c13e25a41498fdda2aa
Daniel Schepers <Daniel@dan-on-mercury.enova.com>
2015-01-20 08:42:18 -0600

    All content removed. Intend to go through making nav bar for workshop 1/20/15

--- app/assets/javascripts/_compare.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  if (urlParams['tickers']){
-    $('.no-stocks.hidden-xs').toggleClass('hidden-xs');
-    $('.no-stocks.visible-xs').toggleClass('visible-xs');
-    $('.no-stocks').toggle();
-  }
-});
